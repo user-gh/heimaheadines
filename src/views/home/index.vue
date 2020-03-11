bc1de767873434aa396c5bdc24ad5774c94bc2df
GH <2515911847@qq.com>
2020-03-11 11:21:01 +0800

    '完成首页模块顶部布局'

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
-     <van-search shape="round" background="#3296fa" placeholder="请输入搜索关键词" />
+    <div class="top-nav">
+      <van-icon class="top-icon" name="wap-nav" />
+      <van-search class="top-search" shape="round" background="#3194ff" placeholder="请输入搜索关键词" />
+      <van-icon class="top-icon"  name="search" />
+    </div>
   </div>
- 
 </template>
 
 <script>
@@ -14,5 +17,24 @@ export default {
 };
 </script>
 
-<style>
+<style lang='less' scoped>
+.home {
+  .top-nav {
+    display: flex;
+    align-items: center;
+    background: #3194ff;
+    padding-left: 10px;
+    padding-right: 10px;
+
+    .top-icon{
+      font-size: 25px;
+      color: #fff;
+    }
+
+    .top-search{
+      flex:1
+    }
+  }
+
+}
 </style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -2,10 +2,10 @@
   <div>
     <router-view />
     <van-tabbar v-model="active">
-      <van-tabbar-item to="/home" icon="home-o">主页</van-tabbar-item>
-      <van-tabbar-item to="/question" icon="comment-o">问答</van-tabbar-item>
-      <van-tabbar-item to="/video" icon="tv-o">视频</van-tabbar-item>
-      <van-tabbar-item to="/mine" icon="contact">我的</van-tabbar-item>
+      <van-tabbar-item name="home" to="/home" icon="home-o">主页</van-tabbar-item>
+      <van-tabbar-item name="question" to="/question" icon="comment-o">问答</van-tabbar-item>
+      <van-tabbar-item name="video" to="/video" icon="tv-o">视频</van-tabbar-item>
+      <van-tabbar-item name="mine" to="/mine" icon="contact">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
@@ -15,7 +15,7 @@ export default {
   name: "index",
   data() {
     return {
-        active:''
+        active:'home'
     };
   }
 };
