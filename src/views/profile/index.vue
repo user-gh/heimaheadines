7f9077d4131c95336b41eaf42c37327c8ba81834
GH <2515911847@qq.com>
2020-03-22 14:08:14 +0800

    '完成所有弹出层的显示和功能'

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -8,21 +8,51 @@
           <img @click="$refs.photo.show = true" class="avatar" :src="userInfo.photo" alt />
         </template>
       </van-cell>
-      <van-cell title="昵称" :value="userInfo.name" is-link />
-      <van-cell title="介绍" :value="userInfo.intro" is-link />
+      <van-cell @click="nameshow = true" title="昵称" :value="userInfo.name" is-link />
+      <van-cell @click="introshow = true" title="介绍" :value="userInfo.intro" is-link />
     </van-cell-group>
     <van-cell-group>
-      <van-cell title="性别" :value="userInfo.gender == 0 ? '男' : '女'" is-link />
-      <van-cell title="生日" :value="userInfo.birthday" is-link />
+      <van-cell
+        @click="gendershow = true"
+        title="性别"
+        :value="userInfo.gender == 0 ? '男' : '女'"
+        is-link
+      />
+      <van-cell @click="datashow = true" title="生日" :value="userInfo.birthday" is-link />
     </van-cell-group>
-    <!-- 弹出层 -->
+    <!-- 选择图片弹出层 -->
     <photo ref="photo" @upOK="changeImage" />
+    <!-- 选择昵称弹出层 -->
+    <van-popup v-model="nameshow" position="bottom">
+      <van-field v-model="userInfo.name" />
+    </van-popup>
+    <!-- 选择介绍弹出层 -->
+    <van-popup v-model="introshow" position="bottom">
+      <van-field v-model="userInfo.intro" />
+    </van-popup>
+    <!-- 选择性别弹出层 -->
+    <van-popup class="gender" v-model="gendershow" position="bottom">
+      <div @click="changeGender(0)" class="gender-item">男</div>
+      <div @click="changeGender(1)" class="gender-item">女</div>
+      <div @click="gendershow = false" class="gender-item">取消</div>
+    </van-popup>
+    <!-- 选择年月日弹出层 -->
+  <van-popup v-model="datashow" position="bottom">
+      <van-datetime-picker 
+      v-model="currentDate" 
+      type="date" 
+      :min-date="minDate" 
+      :max-date="maxDate"
+      @confirm = 'changeDate'
+       />
+  </van-popup>
   </div>
 </template>
 
 <script>
 import { getInfo, getProfile } from "@/api/user";
 import photo from "./commponts/photo";
+import dayjs from 'dayjs'
 export default {
   components: {
     photo
@@ -30,16 +60,36 @@ export default {
   name: "index",
   data() {
     return {
-      userInfo: {}
+      userInfo: {},
+      nameshow: false,
+      introshow: false,
+      gendershow: false,
+      datashow:false,
+      minDate: new Date(1949, 0, 1),
+      maxDate: new Date(),
+      currentDate:new Date()
     };
   },
   methods: {
+    // 把子组件里上传后的图片地赋值给用户信息里的photo
     changeImage(val) {
       // console.log(val);
-      // 把子组件里上传后的图片地赋值给用户信息里的photo
       this.userInfo.photo = val;
       // 把对话框隐藏
       this.$refs.photo.show = false;
+    },
+    // 修改性别并隐藏弹出层
+    changeGender(gender) {
+      this.userInfo.gender = gender;
+      this.gendershow = false;
+    },
+    // 日期改变点击确认的点击事件
+
+    changeDate(){
+      // 把日期对象转成字符串
+      this.userInfo.birthday = dayjs(this.currentDate).format('YYYY-MM-DD');
+      // 关闭弹出框
+      this.datashow = false;
     }
   },
   async created() {
@@ -81,6 +131,18 @@ export default {
       width: 30px;
       height: 30px;
       border-radius: 50%;
+    }
+  }
+  .gender {
+    background: #f8f8f8;
+    .gender-item {
+      background: #fff;
+      text-align: center;
+      padding: 10px 0;
+
+      &:last-child {
+        margin-top: 15px;
+      }
     }
   }
 }
